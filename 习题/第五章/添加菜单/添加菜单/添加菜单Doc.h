a7eebb923612161d96743a2fe249a755405a309b
mubai-victor <1678212538@qq.com>
2020-08-27 02:14:01 +0800

    添加 读取装配关系

---
+++ 习题/第五章/添加菜单/添加菜单/添加菜单Doc.h
@@ -0,0 +1,48 @@
+
+// 添加菜单Doc.h : interface of the C添加菜单Doc class
+//
+
+
+#pragma once
+
+
+class C添加菜单Doc : public CDocument
+{
+protected: // create from serialization only
+	C添加菜单Doc();
+	DECLARE_DYNCREATE(C添加菜单Doc)
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// Implementation
+public:
+	virtual ~C添加菜单Doc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Generated message map functions
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// Helper function that sets search content for a Search Handler
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
